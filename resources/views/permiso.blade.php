6b98b275b0ef389d21819d8ef7ac4c805c6e6c16
Tutorial_Laravel <alex.palacios@unah.hn>
2019-07-29 10:54:05 -0600

    Clase 9, trabajo con rutas

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,3 @@
+<!--Sistemas de Permisos de sistema {{$nombre}} {{$slug}}
+
+<a href="{{route('permiso')}}">Ir a permiso</a>-->
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {  rutear un closure, funcion dentro de otra funcion
     return view('welcome');
-});
+});*/
+
+//Route::get('permiso', 'PermisosController@create'); Rutear un controlador
+//Route::view('permiso', 'permiso'); //pasar una vista
+//Route::get('permiso/{nombre}/{sluug?}', 'PermisosController@index');  //enrutar con parametros y un parametro opcional
+//Route::get('admin/sistema/permisos', 'PermisosController@index')->name('permiso'); //ruta con noombre
+
+Route::get('permiso/{nombre}', function ($nombre) {   //parametoo validado
+    return $nombre; 
+})->where('nombre', '[A-Za-z]+')->name('permiso');
